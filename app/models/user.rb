205a74857d1ff52cb517f4bcff77ead68986b4fb
John Leavell <johnleavell@gmail.com>
2024-04-22 09:57:47 -0500

    updated user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,10 +38,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :bottom_size, presence: true
   validates :top_size, presence: true
-  
+
   has_many  :waivers, class_name: "Waiver", foreign_key: "user_id", dependent: :destroy
   has_many  :user_runs, class_name: "UserRun", foreign_key: "user_id", dependent: :destroy
   belongs_to :shoe, required: true, class_name: "Shoe", foreign_key: "shoe_id"
-
+  has_many :likes, class_name: "Like", foregin_key: "fan_id"
 
 end
