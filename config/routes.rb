10f4e09ce49c01875ed8b0e7c44a019a1382291a
John Leavell <johnleavell@gmail.com>
2024-04-24 15:17:41 -0500

    updated sign out action, cleaned up homepage ui

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :runs
   resources :shoes
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
